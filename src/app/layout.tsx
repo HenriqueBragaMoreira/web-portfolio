5673ee201dba4b74ac50a2030455f3cbb9255d46
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-04-29 17:43:40 -0300

    fix: 🐛 remove warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <ThemeProvider
           attribute="class"
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -62,7 +62,7 @@ export function Navbar() {
       <div className="flex items-center gap-8">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
+            <Button className="focus-visible:ring-0" variant="ghost" size="icon">
               <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
