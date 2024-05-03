958124ed0b51cf413b808ef2ab338cf57bf99bd8
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-05-03 17:33:17 -0300

    feat: 🎸 add new section with all technical skills

--- src/app/[lang]/(home)/components/secondContent.tsx
+++ src/app/[lang]/(home)/components/secondContent.tsx
@@ -1,18 +1,35 @@
 import { TypewriterEffectSmooth } from "@/components/aceternity-ui/typewriter-effect";
+import { stackIcons } from "@/components/icon/stacks";
 import { DictionaryType } from "@/models/translate";
 
 export function SecondContent({ dictionary }: { dictionary: DictionaryType }) {
   return (
     <div className="h-screen bg-foreground/15 py-11" id="second-content">
-      <TypewriterEffectSmooth
-        className="flex justify-center my-0 space-x-0"
-        words={dictionary["second-content"]["title-type-writer"]}
-      />
-      <TypewriterEffectSmooth
-        className="flex justify-center my-0"
-        words={dictionary["second-content"]["subtitle-type-writer"]}
-        cursorClassName="hidden"
-      />
+      <div>
+        <TypewriterEffectSmooth
+          className="flex justify-center my-0 space-x-0"
+          words={dictionary["second-content"]["title-type-writer"]}
+        />
+        <TypewriterEffectSmooth
+          className="flex justify-center my-0"
+          words={dictionary["second-content"]["subtitle-type-writer"]}
+          cursorClassName="hidden"
+        />
+      </div>
+      <div className="grid grid-cols-4 gap-12 mt-12">
+        {stackIcons.map((item) => (
+          <div className="flex flex-col justify-center items-center" key={item.stack_name}>
+            <a
+              href={item.stack_link}
+              target="_blank"
+              className="bg-card dark:bg-foreground rounded-full p-3 transition duration-300 ease-in-out hover:scale-[1.2]"
+            >
+              <item.stack_icon className="size-9" />
+            </a>
+            <p>{item.stack_name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
