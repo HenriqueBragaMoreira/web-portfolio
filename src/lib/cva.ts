a2cabdd959d973c37251199c477998420c274639
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2025-03-14 12:33:47 -0300

    chore: add cva and tailwind-merge

---
+++ src/lib/cva.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cva";
+import { twMerge } from "tailwind-merge";
+
+export const {
+  cva,
+  cx: cn,
+  compose,
+} = defineConfig({
+  hooks: {
+    onComplete: (className) => twMerge(className),
+  },
+});
+
+export type { VariantProps } from "cva";
